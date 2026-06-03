4c49e7a95153f4ac8df272badc440261b952803a
SeniorPinata28 <115035394+SeniorPinata28@users.noreply.github.com>
2026-06-04 05:28:59 +0700

    Add admin page

---
+++ web/app/admin/page.jsx
@@ -0,0 +1,2 @@
+import Header from '../../components/Header';import Footer from '../../components/Footer';import AdminClient from './AdminClient';
+export default function Page(){return <><Header/><main className="main"><AdminClient/></main><Footer/></>}
