a6a5b56c7f68a86cf53943331b0ca3cd60679bd9
SeniorPinata28 <115035394+SeniorPinata28@users.noreply.github.com>
2026-06-04 14:53:26 +0700

    Add Rossko setup page

---
+++ web/app/rossko-setup/page.jsx
@@ -0,0 +1,2 @@
+import Header from '../../components/Header';import Footer from '../../components/Footer';import RosskoSetupClient from './RosskoSetupClient';
+export default function Page(){return <><Header/><main className="main"><RosskoSetupClient/></main><Footer/></>}
