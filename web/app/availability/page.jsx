eec7634ce153b591f64a5e36f7afa4abfe2af739
SeniorPinata28 <115035394+SeniorPinata28@users.noreply.github.com>
2026-06-04 12:12:13 +0700

    Add availability page

---
+++ web/app/availability/page.jsx
@@ -0,0 +1,2 @@
+import Header from '../../components/Header';import Footer from '../../components/Footer';import AvailabilityClient from './AvailabilityClient';
+export default function Page(){return <><Header/><main className="main"><AvailabilityClient/></main><Footer/></>}
